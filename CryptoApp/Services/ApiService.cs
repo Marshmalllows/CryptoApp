2d505944abadb151ba761e3731b9a89d55a2d8d8
Maksym Poliukhovych <maxpolyuhovich@gmail.com>
2025-01-29 14:48:47 +0200

    Add API request and simple visualisation

---
+++ CryptoApp/Services/ApiService.cs
@@ -0,0 +1,17 @@
+using System.Net.Http;
+using System.Text.Json;
+using CryptoApp.Models;
+
+namespace CryptoApp.Services;
+
+public static class ApiService
+{
+    private static readonly HttpClient HttpClient = new();
+
+    public static async Task<AssetsModel> GetAssetsAsync()
+    {
+        const string url = "https://api.coincap.io/v2/assets?limit=10";
+        var response = await HttpClient.GetStringAsync(url);
+        return JsonSerializer.Deserialize<AssetsModel>(response)!;
+    }
+}
--- CryptoApp/ViewModels/HomeViewModel.cs
+++ CryptoApp/ViewModels/HomeViewModel.cs
@@ -1,3 +1,46 @@
+using System.Collections.ObjectModel;
+using CryptoApp.Models;
+using CryptoApp.Services;
+
 namespace CryptoApp.ViewModels;
 
-public class HomeViewModel : BaseViewModel;
+public class HomeViewModel : BaseViewModel
+{
+    private AssetsModel _assetsModel;
+
+    private ObservableCollection<string> _topChartsStrings = [];
+
+    public ObservableCollection<string> TopChartsStrings
+    {
+        get => _topChartsStrings;
+        set
+        {
+            _topChartsStrings = value;
+            OnPropertyChanged(nameof(TopChartsStrings));
+        }
+    }
+
+    public HomeViewModel()
+    {
+        _ = LoadDataAsync();
+    }
+
+    private async Task LoadDataAsync()
+    {
+        _assetsModel = await ApiService.GetAssetsAsync();
+        var data = _assetsModel.Data;
+
+        if (data is null)
+        {
+            return;
+        }
+        
+        TopChartsStrings.Clear();
+        
+        for (var i = 0; i < data.Count; i++)
+        {
+            var coinInfo = $"{i + 1}. {data[i].Name} ({data[i].Symbol}) Price:{data[i].PriceUsd}$";
+            TopChartsStrings.Add(coinInfo);
+        }
+    }
+}
